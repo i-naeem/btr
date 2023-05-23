a4d4c18d7db022fd67b01e86e9e8dc4fd4607b28
Mohammad Naeem <inaeem@protonmail.com>
2023-05-23 16:53:08 +0500

    add scroll

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
+import utils
 import random
 import constants
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from undetected_chromedriver import Chrome, ChromeOptions
 
@@ -16,21 +16,32 @@ ANCHOR_SELECT_BY = HOST.get('anchor_select_by')
 service = Service(executable_path=constants.DRIVER_PATH)
 chrome_options = ChromeOptions()
 
+chrome_options.add_argument("--no-sandbox")
+
+# Add custom headers
+headers = {
+    'Referer': 'https://www.facebook.com/'
+}
+chrome_options.add_argument(f"--headers={headers}")
+
 driver = Chrome(service=service, options=chrome_options)
 driver.maximize_window()
 
 driver.get(START_URL)
 
 
-for i in range(10):
+for i in range(5):
+    utils.scroll_page(driver)
     anchors = driver.find_elements(ANCHOR_SELECT_BY, ANCHOR_SELECTOR)
     anchor = random.choice(anchors)
     href = anchor.get_attribute("href")
     driver.execute_script('arguments[0].scrollIntoView()', anchor)
     anchor.click()
-    time.sleep(3)
-    print(f'VIEW: [{href}]')
+    time.sleep(2)
+    utils.scroll_page(driver)
+    time.sleep(1)
     driver.back()
+
 
 input('Press any key to qiut')
 driver.quit()
--- utils.py
+++ utils.py
@@ -5,9 +5,13 @@ random_ua(): Returns a random user agent string.
 normalize_url(url): Normalize url by removing extra parameters from the url.
 """
 
+import time
 import random
 from constants import USER_AGENTS
 from urllib.parse import urljoin, urlparse
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 def random_ua() -> str:
@@ -38,3 +42,15 @@ def normalize_url(url: str) -> str:
     normalized_url = urljoin(url, path)
 
     return str(normalized_url)
+
+
+def scroll_page(driver: WebDriver, speed: float = 10) -> None:
+    scroll_height = driver.execute_script("return document.body.scrollHeight")
+
+    # Create an instance of ActionChains
+    actions = ActionChains(driver)
+
+    # Perform the scroll action until the end of the page is reached
+    while driver.execute_script("return window.pageYOffset + window.innerHeight") < scroll_height:
+        actions.send_keys(Keys.SPACE).perform()
+        time.sleep(1)
