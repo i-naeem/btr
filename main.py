14b0f2a598e1c70220456dbc41cff6842c00d1f6
Mohammad Naeem <inaeem@protonmail.com>
2023-05-24 13:51:03 +0500

    add bing search

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ ANCHOR_SELECTOR = HOST.get('anchor_selector')
 ANCHOR_SELECT_BY = HOST.get('anchor_select_by')
 
 
-driver = search.duckduckgo("site:merjob.com", "merjob.com")
+driver = search.bing("site:merjob.com", "merjob.com")
 
 
 for i in range(5):
--- search.py
+++ search.py
@@ -14,26 +14,51 @@ chrome_options = ChromeOptions()
 
 chrome_options.add_argument("--no-sandbox")
 driver = Chrome(service=service, options=chrome_options)
-
-
-def bing(keywords: str, host: str):
-    start_url = "https://google.com"
-    driver.get(start_url)
-    driver.maximize_window()
-    pass
+driver.start_session()
+driver.maximize_window()
 
 
 def google(keywords: str, host: str):
     start_url = "https://bing.com"
     driver.get(start_url)
-    driver.maximize_window()
     pass
+
+
+def bing(keywords: str, host: str):
+    start_url = "https://bing.com"
+    driver.get(start_url)
+    time.sleep(1)
+    searchbar = driver.find_element(by=By.ID, value="sb_form_q")
+
+    for key in keywords:
+        searchbar.send_keys(key)
+        time.sleep(random.uniform(*KEY_DELAY_RANGE))
+
+    searchbar.send_keys(Keys.ENTER)
+
+    items = []
+    search_items = driver.find_elements(by=By.CSS_SELECTOR, value=".b_algo h2")
+
+    for item in search_items:
+        anchor = item.find_element(by=By.TAG_NAME, value="a")
+        href = anchor.get_attribute('href')
+        if href.find(host) != -1:
+            items.append(item)
+
+    random_item = random.choice(items)
+    random_item.click()
+    time.sleep(1)
+
+    utils.scroll_page(driver)
+
+    _root = driver.find_element(by=By.CSS_SELECTOR, value='a[rel=home]')
+    _root.click()
+    time.sleep(1)
+    return driver
 
 
 def duckduckgo(keywords: str, host: str):
     start_url = "https://duckduckgo.com"
-    driver.start_session()
-    driver.maximize_window()
     driver.get(start_url)
 
     searchbar = None
