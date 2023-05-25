25516123415456f63de08051fb079c0c6fbe1f8f
Mohammad Naeem <inaeem@protonmail.com>
2023-05-25 12:16:31 +0500

    add SearchBot

---
+++ SearchBot.py
@@ -0,0 +1,23 @@
+
+class SearchBot:
+    def __init__(self,
+                 name,
+                 start_url,
+                 input_selector,
+                 search_result_selector,
+                 ):
+        self.name = name;
+        self.start_url = start_url;
+        self.input_selector = input_selector;
+        self.search_result_selector = search_result_selector;
+    
+    
+    def search(self):
+        pass
+    
+    def get_search_results(self):
+        pass
+    
+    
+        
+        
