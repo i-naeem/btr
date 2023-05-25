2243e67e5397d10bbde5a68ad7a45dd2275550f6
Mohammad Naeem <inaeem@protonmail.com>
2023-05-25 13:42:38 +0500

    Anontate functions and add docstring

--- SearchBot.py
+++ SearchBot.py
@@ -1,9 +1,12 @@
 import env
+from typing import List
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+
 
 
 service = Service(executable_path=env.CHROME_EXECUTABLE_PATH)
@@ -28,7 +31,17 @@ class SearchBot:
         self.search_result_selected_by = search_result_selected_by;
     
     
-    def search(self, search_text:str):
+    def search(self, query:str) -> List[WebElement]:
+        """
+        Open the driver and search the query in search engine and returns the results from search engine.
+
+        Args:
+            query (str): query text that would be search in search engine.
+
+        Returns:
+            List[WebElement]: list of search results.
+        """
+        
         self.driver.get(self.start_url)
             
         searchbar = self.driver.find_element(
@@ -36,7 +49,7 @@ class SearchBot:
             self.searchbar_selector
         )
         
-        for char in search_text:
+        for char in query:
             searchbar.send_keys(char)
         searchbar.send_keys(Keys.ENTER)
         
