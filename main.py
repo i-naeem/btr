0b42946b1c6a950aabb6b254ed8cadea15460a52
Mohammad Naeem <inaeem@protonmail.com>
2023-05-25 18:12:38 +0500

    Setup proxy

--- main.py
+++ main.py
@@ -8,35 +8,42 @@ from SearchEngineConfigs import GOOGLE_CONFIGS
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.chrome.service import Service
 
-service = Service(executable_path=env.CHROME_EXECUTABLE_PATH)
-chrome_options = ChromeOptions()
-driver = Chrome(service=service, options=chrome_options)
-driver.maximize_window()
-driver.implicitly_wait(5)
+
+for proxy in PROXIES:
+    
+    service = Service(executable_path=env.CHROME_EXECUTABLE_PATH)
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument(f'--proxy-server={proxy.protocol}://{proxy.server}:{proxy.port}')
+    driver = Chrome(service=service, options=chrome_options)
+    driver.maximize_window()
+    driver.implicitly_wait(5)
+    
 
 
-def f(el):
-    return el.get_attribute('href').lower().find('techwispy.com') != -1
+    def f(el):
+        return el.get_attribute('href').lower().find('techwispy.com') != -1
 
-google = SearchBot(driver=driver, **GOOGLE_CONFIGS)
-search_results = google.search(
-    query="site:techwispy.com", 
-    fltr=f
-)
+    google = SearchBot(driver=driver, **GOOGLE_CONFIGS)
+    search_results = google.search(
+        query="site:techwispy.com", 
+        fltr=f
+    )
 
 
-selectors = [
-    (By.CSS_SELECTOR, ".wp-block-latest-posts__post-title"),
-    (By.CSS_SELECTOR, ".entry-title > a")
-]
+    selectors = [
+        (By.CSS_SELECTOR, ".wp-block-latest-posts__post-title"),
+        (By.CSS_SELECTOR, ".entry-title > a")
+    ]
 
-bot = TrafficBot(
-    selectors=selectors,
-    pages=search_results,
-    driver=driver,
-    views=3
-)
+    bot = TrafficBot(
+        selectors=selectors,
+        pages=search_results,
+        driver=driver,
+        views=3
+    )
 
-bot.start()
+
+
+    bot.start()
 
 input("Press enter to quit")
