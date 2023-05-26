3655cd600ca25d9613a50d2cec17689ed313988f
Mohammad Naeem <inaeem@protonmail.com>
2023-05-26 07:14:26 +0500

    use bing for searches

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 
 import env
+import random
 from SearchBot import SearchBot
 from TrafficBot import TrafficBot
 from proxies import PROXIES, Proxy
 from joblib import Parallel, delayed
 from selenium.webdriver.common.by import By
-from SearchEngineConfigs import GOOGLE_CONFIGS
-from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver import DesiredCapabilities
+from undetected_chromedriver import Chrome, ChromeOptions
 from selenium.webdriver.chrome.service import Service
+from SearchEngineConfigs import GOOGLE_CONFIGS, BING_CONFIGS
 
 
 def start_bot(proxy: Proxy):    
     service = Service(executable_path=env.CHROME_EXECUTABLE_PATH)
     chrome_options = ChromeOptions()
     chrome_options.add_argument(f'--proxy-server={proxy.protocol}://{proxy.server}:{proxy.port}')
+
     driver = Chrome(service=service, options=chrome_options)
     driver.maximize_window()
     driver.implicitly_wait(5)
@@ -23,7 +26,7 @@ def start_bot(proxy: Proxy):
     def f(el):
         return el.get_attribute('href').lower().find('techwispy.com') != -1
 
-    google = SearchBot(driver=driver, **GOOGLE_CONFIGS)
+    google = SearchBot(driver=driver, **BING_CONFIGS)
     search_results = google.search(
         query="site:techwispy.com", 
         fltr=f
@@ -48,4 +51,4 @@ def start_bot(proxy: Proxy):
     except:
         print("FAILED!!!")
 
-Parallel(n_jobs=2)(delayed(start_bot)(proxy) for proxy in PROXIES)
+Parallel(n_jobs=1)(delayed(start_bot)(proxy) for proxy in random.sample(PROXIES, 2))
