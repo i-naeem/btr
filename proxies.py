1376ede48b7153c1715733be2f35dc8bac8268b4
Mohammad Naeem <inaeem@protonmail.com>
2023-05-26 07:24:55 +0500

    Update proxies

--- proxies.py
+++ proxies.py
@@ -1,4 +1,4 @@
-# WebShare.io jude.10@mailtouiq.com:123password123
+# WebShare.io demian3@mailtouiq.com:123password123
 # The proxies are authenticated by IP Address so it will not work on someone else pc
 # Get the trail version from WebShare.io and use your own proxies instead of these 
 from dataclasses import dataclass
@@ -11,14 +11,14 @@ class Proxy:
     port: str;
 
 PROXIES = [
-    Proxy(protocol="http", server="2.56.119.93", port="5074"),
-    Proxy(protocol="http", server="45.94.47.66", port="8110"),
-    Proxy(protocol="http", server="188.74.183.10", port="8279"),
-    Proxy(protocol="http", server="188.74.210.21", port="6100"),
-    Proxy(protocol="http", server="45.155.68.129", port="8133"),
-    Proxy(protocol="http", server="154.95.36.199", port="6893"),
-    Proxy(protocol="http", server="185.199.231.45", port="8382"),
-    Proxy(protocol="http", server="188.74.210.207", port="6286"),
-    Proxy(protocol="http", server="185.199.229.156", port="7492"),
-    Proxy(protocol="http", server="185.199.228.220", port="7300"),
+    Proxy(protocol="socks5", server="185.199.229.156", port="7492"),
+    Proxy(protocol="socks5", server="185.199.228.220", port="7300"),
+    Proxy(protocol="socks5", server="185.199.231.45", port="8382"),
+    Proxy(protocol="socks5", server="188.74.210.207", port="6286"),
+    Proxy(protocol="socks5", server="188.74.183.10", port="8279"),
+    Proxy(protocol="socks5", server="188.74.210.21", port="6100"),
+    Proxy(protocol="socks5", server="45.155.68.129", port="8133"),
+    Proxy(protocol="socks5", server="154.95.36.199", port="6893"),
+    Proxy(protocol="socks5", server="2.56.119.93", port="5074"),
+    Proxy(protocol="socks5", server="45.94.47.66", port="8110")
 ]
