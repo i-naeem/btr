edbdb85b5a363004ac994c717c18fa735b35f7b7
Mohammad Naeem <inaeem@protonmail.com>
2023-05-26 07:14:55 +0500

    Add bing search engine configs

--- SearchEngineConfigs.py
+++ SearchEngineConfigs.py
@@ -5,6 +5,13 @@ GOOGLE_CONFIGS = {
     "start_url":"https://google.com",
     "searchbar_selector":"textarea",
     "search_result_selector":"//a[h3]",
-    "search_result_selected_by": By.XPATH
+    "search_result_selected_by": By.XPATH,
+}
+
+BING_CONFIGS =  {
+    "name":"Bing",
+    "start_url":"https://bing.com",
+    "searchbar_selector":"#sb_form_q",
+    "search_result_selector":".b_algo > h2 > a",
 }
 
