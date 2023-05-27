87319e5d6b6e903a0f0f10445c358e2606346b14
Mohammad Naeem <inaeem@protonmail.com>
2023-05-27 13:56:27 +0500

    Add default value for chrome driver

--- env.py
+++ env.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CHROME_EXECUTABLE_PATH = os.environ.get('CHROME_EXECUTABLE_PATH')
+CHROME_EXECUTABLE_PATH = os.environ.get('CHROME_EXECUTABLE_PATH') or "./assets/chromedriver.exe"
 
 if __name__ == '__main__':
     print(CHROME_EXECUTABLE_PATH)
