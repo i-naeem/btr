c1bd8bb97a1ddca5e37488c66170c9d84dbf353c
Mohammad Naeem <inaeem@protonmail.com>
2023-05-27 22:49:53 +0500

    Remove python files

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-CHROME_EXECUTABLE_PATH = os.environ.get('CHROME_EXECUTABLE_PATH') or "./assets/chromedriver.exe"
-
-if __name__ == '__main__':
-    print(CHROME_EXECUTABLE_PATH)
