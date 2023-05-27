8ef281369928c31afe76446039688458696f5af8
Mohammad Naeem <inaeem@protonmail.com>
2023-05-27 13:56:05 +0500

    Add comments and fix to start the enviroment before installing packages

--- set-env.bat
+++ set-env.bat
@@ -1,3 +1,11 @@
-mkdir files
+@REM Creates assets directory
+mkdir assets
+
+@REM Creates a virtual environment for the project
 python -m venv btrenv
+
+@REM Actibvate the virtual env
+activate.bat
+
+@REM Install the required packages
 pip install -r requirements.txt
