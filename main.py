1490016801d927488a989037096f9a302ddcabbd
Mohammad Naeem <inaeem@protonmail.com>
2023-05-28 15:33:59 +0500

    Add main script to test bot

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+from Bot import Bot
+from models import Selector
+from selenium.webdriver.common.by import By
+from search_controller import SearchController
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from undetected_chromedriver import Chrome, ChromeOptions
+
+service = Service(executable_path="./assets/chromedriver.exe")
+options = ChromeOptions()
+options.page_load_strategy = 'eager'
+driver = Chrome(service=service, options=options)
+driver.maximize_window()
+driver.get('https://bing.com')
+
+result_selectors = [
+    Selector(by=By.CSS_SELECTOR, value="h2 > a")
+]
+
+controller = SearchController(driver=driver, search_result_selectors=result_selectors)
+items = controller.search(q="site:yourfabulouslives.com")
+
+bot = Bot(
+    driver=driver,
+    available_pages=items,
+    selectors=[(By.CSS_SELECTOR, 'h2 > a')],
+    max_tabs=4,
+    max_views=8
+)
+
+bot.start()
