a9b92f5a940ce2da012dcd3047576e992b4ae207
Mohammad Naeem <inaeem@protonmail.com>
2023-05-28 18:02:03 +0500

    Create a main function to start the bot

--- main.py
+++ main.py
@@ -1,39 +1,53 @@
+import env
+import utils
 import random
 from Bot import Bot
 from models import Selector
 from selenium.webdriver.common.by import By
 from search_controller import SearchController
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.wait import WebDriverWait
 from undetected_chromedriver import Chrome, ChromeOptions
 
-service = Service(executable_path="./assets/chromedriver.exe")
-options = ChromeOptions()
-options.page_load_strategy = 'eager'
-driver = Chrome(service=service, options=options)
-driver.maximize_window()
-driver.get('https://bing.com')
-
-result_selectors = [
-    Selector(by=By.CSS_SELECTOR, value="h2 > a")
+search_engines = [
+    {
+        "start_url": "https://bing.com",
+        "selector": Selector(by=By.CSS_SELECTOR, value="h2 > a"),
+    },
+    {
+        "start_url": "https://duckduckgo.com",
+        "selector": Selector(by=By.CSS_SELECTOR, value="h2 > .result-title-a"),
+    }
 ]
 
-controller = SearchController(driver=driver, search_result_selectors=result_selectors)
-items = controller.search(q="site:merjob.com")
 
-filtered_items = [
-    item for item in items if item.get_attribute('href').find('merjob.com') != -1
-]
+def main(proxy):
+    service = Service(executable_path=env.CHROME_EXECUTABLE_PATH)
+    chrome_options = utils.get_chrome_options(proxy)
+    chrome_options.page_load_strategy = 'eager'
+    driver = Chrome(service=service, options=chrome_options)
+    driver.maximize_window()
 
-bot = Bot(
-    driver=driver,
-    available_pages=filtered_items,
-    selectors=[
-        Selector(By.CLASS_NAME, 'wp-block-latest-posts__post-title'),
-        Selector(By.CSS_SELECTOR, '.entry-title > a')
-    ],
-    max_tabs=5,
-    max_views=50
-)
+    search_engine = random.choice(search_engines)
+    driver.get(search_engine.get("start_url"))
 
-bot.start()
+    controller = SearchController(
+        driver=driver, search_result_selectors=search_engine.get("selector")
+    )
+
+    items = controller.search(q="site:merjob.com")
+    filtered_items = [
+        item for item in items if item.get_attribute('href').find('merjob.com') != -1
+    ]
+
+    bot = Bot(
+        driver=driver,
+        available_pages=filtered_items,
+        selectors=[
+            Selector(By.CLASS_NAME, 'wp-block-latest-posts__post-title'),
+            Selector(By.CSS_SELECTOR, '.entry-title > a')
+        ],
+        max_tabs=5,
+        max_views=4
+    )
+
+    bot.start()
