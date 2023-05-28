5fc17b32e243c168d493ba25faa71f13afd9931a
Mohammad Naeem <inaeem@protonmail.com>
2023-05-28 11:09:09 +0500

    Fix selector not json serializable error and test controller

--- search_controller.py
+++ search_controller.py
@@ -5,7 +5,10 @@ from selenium.webdriver.common.by import By;
 from models import Selector
 from typing import List
 
-DEFAULT_SEARCHBAR_SELECTOR = Selector(by=By.CSS_SELECTOR, value="input[type='search']")
+from undetected_chromedriver import Chrome;
+from selenium.webdriver.chrome.service import Service;
+
+DEFAULT_SEARCHBAR_SELECTOR = Selector(by=By.NAME, value="q")
 DEFAULT_SEARCH_RESULT_SELECTORS = [Selector(by=By.TAG_NAME, value="a")]
 
 class SearchController:
@@ -16,30 +19,40 @@ class SearchController:
                 ):
         self.search_result_selectors = search_result_selectors;
         self.searchbar_selector = searchbar_selector;
-        self.search_results = []
         self.driver = driver;
 
         self._q = None;
         self._searchbar = None;
+        self.search_results = []
     
     
     def search(self, q: str):
+        self._q = q;
         self._find_searchbar();
         self._searchbar.send_keys(q, Keys.ENTER)
         
         return self._find_search_results()
     
     def _find_searchbar(self) -> None:
-        self._searchbar = self.driver.find_element(self.searchbar_selector)
+        self._searchbar = self.driver.find_element(
+            self.searchbar_selector.by, self.searchbar_selector.value
+        )
     
     def _find_search_results(self) -> List[WebElement]:
         results = []
         for selector in self.search_result_selectors:
-            results.extend(self.driver.find_elements(*selector))
+            results.extend(self.driver.find_elements(selector.by, selector.value))
         
         return self.search_results;
     
 
 
 if __name__ == "__main__":
-    controller = SearchController(driver=None)
+    driver = Chrome(service=Service(executable_path='./assets/chromedriver.exe'))
+    driver.maximize_window()
+    driver.get('https://bing.com')
+    controller = SearchController(driver=driver)
+    
+    for item in controller.search('Hello World'):
+        print(item.text)
+    
