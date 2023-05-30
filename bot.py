63a67e581cb49d68226466f52521e14020c8f9b1
Mohammad Naeem <inaeem@protonmail.com>
2023-05-30 11:15:51 +0500

    add logging to Bot

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from utils import use_driver, scroll_to_element, scroll_down, scroll_up
+from utils import use_driver, scroll_to_element, scroll_down, scroll_up, use_logging
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
@@ -6,37 +6,45 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 import random
 
+logger = use_logging()
+
 
 class Bot:
     def __init__(self,
                  selectors,
                  driver: WebDriver):
+        logger.info('Creating the instance of Bot.')
+
         self.driver = driver
         self.selectors = selectors
+        self.original_window = self.driver.current_window_handle
 
         self.wait = WebDriverWait(self.driver, 10)
         self.pages = self._find_pages()
         self.ads = self._find_ads()
-
-        self.original_window = self.driver.current_window_handle
-        self.next_page = random.choice(self.pages) if len(self.pages) else None
 
     @property
     def all_tabs(self):
         return [w for w in self.driver.window_handles if w != self.original_window]
 
     def start(self):
+        logger.info('Starting the bot and opening links')
         # Opens Random 5 Pages in New Tab
         for _ in range(5):
+
             element = random.choice(self.pages)
             scroll_to_element(self.driver, element)
+
+            logger.info(f'Opening [{element.get_attribute("href")}] in new tab.')
             element.send_keys(Keys.CONTROL, Keys.ENTER)
 
         self.view()
 
     def view(self):
         # We view all the tabs one by one.
+        logger.info('Viewing the opened tabs.')
         for window in self.all_tabs:
+            logger.info(f'Switching to [{window}] tab.')
             self.driver.switch_to.window(window)
             scroll_down(self.driver)
             scroll_up(self.driver)
@@ -45,24 +53,29 @@ class Bot:
         # Check if there more than one tab open then we switch randomly.
         if len(self.all_tabs) != 0:
             self.original_window = random.choice(self.all_tabs)
+            logger.info(f'Changed the original window to [{self.original_window}]')
             for window in self.all_tabs:
+                logger.info(f'Closing other opened tabs.')
                 self.driver.switch_to.window(window)
                 self.driver.close()
 
+            logger.info(f'Switching to [{self.original_window}]')
             self.driver.switch_to.window(self.original_window)
         # else we stay on the original window scroll up and down.
         else:
+            logger.info(f'No opened tabs were found so scrolling through orignal window.')
             scroll_down(self.driver)
             scroll_up(self.driver)
             scroll_down(self.driver)
 
     def _find_pages(self):
         elements = []
+        logger.info(f'Findin pages on {self.original_window}')
         for selector in self.selectors:
             try:
                 elements.extend(self.wait.until(EC.presence_of_all_elements_located(selector)))
             except TimeoutException:
-                print(f"{selector} elements were not found")
+                logger.error(f'There were no elements found for [ {selector = } ]')
 
         return elements
 
