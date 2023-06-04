883e566d7285823307d8c04ec807080d250fda4c
Mohammad Naeem <inaeem@protonmail.com>
2023-06-04 14:27:48 +0500

    add proxies path

--- constants.py
+++ constants.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 LOGGER = "BTR"
+PROXIES_FILE_PATH = "./assets/proxies.json"
+
 DEFAULT_AD_SELECTORS = [
     (By.CSS_SELECTOR, 'a[href*="adclick"]'),
     (By.CSS_SELECTOR, 'a[href*="doubleclick"]'),
