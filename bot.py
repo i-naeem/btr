8c9d6940fbc2f2f51aa6a2eaba77cf0fa98b44aa
Mohammad Naeem <inaeem@protonmail.com>
2023-06-11 11:20:52 +0500

    add goto and goto_ad method

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from utils.scrolls import scroll_down, scroll_up
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from utils.driver import find_by_selectors
 from typing import List, Tuple
@@ -19,11 +20,20 @@ class Bot:
                  ):
         self.driver = driver
         self.scroll_pause = scroll_pause
-        self.route_selectors = route_selectors
         self.ad_selectors = ad_selectors
+        self.route_selectors = route_selectors
 
         self.available_ads = None
         self.available_routes = self.__find_routes()
+
+        # The window where we clicked on the ad.
+        self.ad_window = None
+        # The window we will switch to.
+        self.next_window = None
+        # The window which was used to open tabs.
+        self.traversing_window = self.driver.current_window_handle
+        # The starting window of the bot.
+        self.original_window = self.driver.current_window_handle
 
     def scroll(self, direction: str = DOWN):
         if direction == DOWN:
@@ -32,10 +42,21 @@ class Bot:
             scroll_up(self.driver, self.scroll_pause)
 
     def goto(self):
-        pass
+        random.shuffle(self.available_routes)
+
+        for route in random.sample(self.available_routes, self.max_tabs):
+            route.send_keys(Keys.CONTROL, Keys.ENTER)
+        self.traversing_window = self.driver.current_window_handle
 
     def goto_ad(self):
-        pass
+        ad = random.choice(self.available_ads)
+        frame = getattr(ad, 'iframe', None)
+        anchor = getattr(ad, 'anchor', None)
+
+        self.driver.switch_to.frame(frame)
+        anchor.click()
+
+        self.ad_window = self.driver.current_window_handle
 
     def __find_ads(self):
         all_ads = []
