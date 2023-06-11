c4fc192eb972f400f4a76b9fd512d82846136136
Mohammad Naeem <inaeem@protonmail.com>
2023-06-11 10:43:48 +0500

    Add find_by_selectors function

--- utils/driver.py
+++ utils/driver.py
@@ -1,5 +1,8 @@
 from selenium.webdriver.chrome.service import Service
 from seleniumwire.undetected_chromedriver import ChromeOptions, Chrome
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 
 def use_driver():
@@ -8,3 +11,22 @@ def use_driver():
     so = dict()
 
     return Chrome(service=s, options=o, seleniumwire_options=so)
+
+
+def find_by_selectors(driver, selectors, timeout=5, max_element=5):
+    wait = WebDriverWait(driver, timeout)
+
+    all_elements = []
+    for selector in selectors:
+        if len(all_elements) > max_element:
+            return all_elements
+        try:
+            elements = wait.until(EC.presence_of_all_elements_located(selector))
+            all_elements.extend(elements)
+        except TimeoutException as e:
+            print(f'TIMEOUT: [{selector}]')
+
+    if len(all_elements) == 0:
+        raise NoSuchElementException(msg="There is no element present for your selector.")
+
+    return all_elements
