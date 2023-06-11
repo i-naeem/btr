3b14c1e1285cecfabf8a8a914ce594de99374de3
Mohammad Naeem <inaeem@protonmail.com>
2023-06-11 10:10:47 +0500

    Setting up bot

---
+++ HumanBot.py
@@ -0,0 +1,19 @@
+import random
+
+UP = "UP"
+DOWN = "DOWN"
+
+
+class Bot:
+    def __init__(self, driver, scroll_speed):
+        self.driver = driver
+        self.scroll_speed = scroll_speed
+
+        self.anchors = None
+
+    def scroll(self, direction: str = DOWN):
+        pass
+
+    def click(self):
+        anchor = random.choice(self.anchors)
+        anchor.click()
