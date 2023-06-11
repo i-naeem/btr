dbd01799f820cada0ff3f1b8c55841d2f6092075
Mohammad Naeem <inaeem@protonmail.com>
2023-06-11 12:24:23 +0500

    Setup basic logging

--- __main__.py
+++ __main__.py
@@ -1,9 +1,24 @@
+from selenium.webdriver.remote.remote_connection import LOGGER
 from selenium.webdriver.common.by import By
 from utils.driver import use_driver
 from bot import Bot
+import logging
 
+
+logging.basicConfig(filename="btr.log", level=logging.INFO)
+
+# Turning of selenium logger
+LOGGER.setLevel(logging.WARNING)
+
+start_url = 'https://books.toscrape.com'
+
+logging.info('Starting driver...')
 driver = use_driver()
-driver.get('https://books.toscrape.com')
+
+logging.info(f'Opening the {start_url}...')
+driver.get(start_url)
+
+logging.info('Creating bot...')
 bot = Bot(
     max_tabs=2,
     driver=driver,
@@ -11,4 +26,5 @@ bot = Bot(
     route_selectors=[(By.CSS_SELECTOR, '.nav.nav-list li a')]
 )
 
+logging.info('Start Crawling...')
 bot.crawling()
