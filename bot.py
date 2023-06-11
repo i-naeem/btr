e1e6a88f66ddf999a3ef5b327c14598a0719b0ae
Mohammad Naeem <inaeem@protonmail.com>
2023-06-11 11:39:01 +0500

    Add start method

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.common.exceptions import NoSuchWindowException
 from utils.scrolls import scroll_down, scroll_up
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -14,14 +15,19 @@ AD_SELECTORS: List[Tuple[str, str]] = [(By.CSS_SELECTOR, ".ad")]
 class Bot:
     def __init__(self,
                  driver: WebDriver,
-                 scroll_pause,
-                 route_selectors,
-                 ad_selectors=AD_SELECTORS,
+                 route_selectors: List[Tuple[str, str]],
+
+                 max_tabs: int = 2,
+                 max_traverse: int = 3,
+                 scroll_pause: float = None,
+                 ad_selectors: List[Tuple[str, str]] = AD_SELECTORS,
                  ):
-        self.driver = driver
+        self.route_selectors = route_selectors
         self.scroll_pause = scroll_pause
         self.ad_selectors = ad_selectors
-        self.route_selectors = route_selectors
+        self.max_traverse = max_traverse
+        self.max_tabs = max_tabs
+        self.driver = driver
 
         self.available_ads = None
         self.available_routes = self.__find_routes()
@@ -35,6 +41,45 @@ class Bot:
         # The starting window of the bot.
         self.original_window = self.driver.current_window_handle
 
+    @property
+    def new_tabs(self):
+        windows = [w for w in self.driver.window_handles
+                   if w != self.traversing_window and w != self.original_window]
+        random.shuffle(windows)
+        return windows
+
+    def crawling(self):
+        for _ in range(self.max_traverse):
+            self.start()
+
+    def start(self):
+        self.scroll(direction=DOWN)
+        self.scroll(direction=UP)
+        self.goto()
+
+        for window in self.new_tabs:
+            self.driver.switch_to.window(window)
+            self.scroll(direction=DOWN)
+            self.scroll(direction=UP)
+
+        self.next_window = random.choice(self.new_tabs)
+        self.driver.switch_to.window(self.next_window)
+
+        self.close_windows()
+
+    def close_windows(self):
+        for window in self.driver.window_handles:
+            if window != self.next_window:
+                try:
+                    self.driver.switch_to.window(window)
+                    self.driver.close()
+                except NoSuchWindowException as e:
+                    pass
+
+        self.original_window = None
+        self.next_window = None
+        self.ad_window = None
+
     def scroll(self, direction: str = DOWN):
         if direction == DOWN:
             scroll_down(self.driver, self.scroll_pause)
@@ -43,9 +88,9 @@ class Bot:
 
     def goto(self):
         random.shuffle(self.available_routes)
-
         for route in random.sample(self.available_routes, self.max_tabs):
             route.send_keys(Keys.CONTROL, Keys.ENTER)
+
         self.traversing_window = self.driver.current_window_handle
 
     def goto_ad(self):
