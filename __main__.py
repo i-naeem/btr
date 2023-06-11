adad1b89e389c243de978363cc0f0a1614efff4c
Mohammad Naeem <inaeem@protonmail.com>
2023-06-11 12:14:16 +0500

    add main script

--- __main__.py
+++ __main__.py
@@ -1,1 +1,13 @@
-print('Hello World')
+from utils.driver import use_driver
+from bot import Bot
+
+driver = use_driver()
+driver.get('https://books.toscrape.com')
+bot = Bot(
+    max_tabs=2,
+    driver=driver,
+    max_traverse=2,
+    route_selectors=[(By.CSS_SELECTOR, '.nav.nav-list li a')]
+)
+
+bot.crawling()
