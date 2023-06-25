b3ade992d54351aabe4edf17e9477169e129ffad
Muhammad Naeem <inaeem@protonmail.com>
2023-06-25 17:01:15 +0500

    Add `chrome_executable_path`

--- utils/create_driver.py
+++ utils/create_driver.py
@@ -1,15 +1,20 @@
 """
 create_driver.py - Create the driver with common settings
 """
-from selenium.webdriver import Chrome, ChromeOptions
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import Chrome, ChromeOptions
+from configs import DRIVER_EXECUTABLE_PATH
 
 
 def create_driver() -> WebDriver:
-    service = Service(executable_path="")
+    service = Service(executable_path=DRIVER_EXECUTABLE_PATH)
     chrome_options = ChromeOptions()
 
-    driver = Chrome(service=service, options=chrome_options)
+    driver = Chrome(
+        service=service,
+        options=chrome_options,
+        # driver_executable_path=DRIVER_EXECUTABLE_PATH
+    )
 
     return driver
