cf847508fe60a28d4270167792393c8b43b63d04
Muhammad Naeem <inaeem@protonmail.com>
2023-06-25 14:42:04 +0500

    Add `__close_windows` method

--- btr.py
+++ btr.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import NoSuchWindowException
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.remote.webdriver import WebDriver
 from utils.find_elements import find_by_selectors
@@ -23,8 +24,27 @@ class BTR:
         self.selectors = selectors
         self.initial_anchors = initial_anchors
 
+        self.next_window = self.driver.current_window_handle
+        self.start_window = self.driver.current_window_handle
+
         self.PAUSE_TIMES = [0.5, 1.0, 1.5, 2]
         self.SCROLL_PAUSE_TIMES = [0.3, 0.5, 0.8]
+
+    def __close_windows(self):
+        for window in self.driver.window_handles:
+            if window == self.next_window:
+                continue
+            try:
+                self.driver.switch_to.window(window)
+                self.__pause()
+                self.driver.close()
+            except NoSuchWindowException as e:
+                print('NoSuchWindow')
+                pass
+
+        self.driver.switch_to.window(self.next_window)
+        self.start_window = self.next_window
+        self.next_window = None
 
     def __scroll(self, direction=SCROLL_DOWN):
         scroll_pause = random.choice(self.SCROLL_PAUSE_TIMES)
