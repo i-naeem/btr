c4e53339a9577531e714b4ca30314a0b344cba55
Muhammad Naeem <inaeem@protonmail.com>
2023-06-25 14:14:45 +0500

    Add `initial_anchors`, `__find_*` methods

--- btr.py
+++ btr.py
@@ -3,7 +3,16 @@ class BTR:
     def __init__(self,
                  driver,
                  selectors,
+                 initial_anchors,
                  ):
 
         self.driver = driver
         self.selectors = selectors
+        self.initial_anchors = initial_anchors
+
+    def __find_anchors(self):
+
+        return []
+
+    def __find_advertisement(self):
+        return []
