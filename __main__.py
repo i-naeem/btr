5189866c931e930a42124fb3527ef8b3cb763031
Muhammad Naeem <inaeem@protonmail.com>
2023-06-25 19:17:12 +0500

    add `site.py`

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 from configs import USER_AGENTS_FILE_PATH
 from utils.parse_json import parse_json
 from configs import PROXIES_FILE_PATH
+from sites import blog_dera_jobs
 
 
 user_agents = parse_json(USER_AGENTS_FILE_PATH)
---
+++ sites.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+from dataclasses import dataclass
+from typing import Tuple
+from typing import List
+
+
+@dataclass
+class Site:
+    site_name: str
+    start_urls: List[Tuple[str, str]]
+    search_selectors: List[Tuple[str, str]]
+    anchors_selectors: List[Tuple[str, str]]
+
+
+blog_dera_jobs = Site(
+    site_name="BlogDeraJobs",
+    start_urls=[
+        ("GoogleSite", "https://sites.google.com/view/linkedin-business-manager/home"),
+        ("DuckDuckGo", "https://duckduckgo.com/?q=site:https://blog.derajobs.pk"),
+        ("Bing", "https://bing.com/?q=site:https://blog.derajobs.pk"),
+    ],
+    search_selectors=[(By.CSS_SELECTOR, "a[href*='https://blogderajobs.com']")],
+    anchors_selectors=[
+        (By.CSS_SELECTOR, ".wp-block-latest-posts__post-title"),
+        (By.CSS_SELECTOR, ".entry-title a"),
+    ]
+)
