f7dcd091462fae7ad5cf445faeedc07835dc4397
Muhammad Naeem <inaeem@protonmail.com>
2023-06-25 13:44:23 +0500

    add code to `create_drvier`

--- utils/create_driver.py
+++ utils/create_driver.py
@@ -1,3 +1,15 @@
 """
 create_driver.py - Create the driver with common settings
 """
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+def create_driver() -> WebDriver:
+    service = Service(executable_path="")
+    chrome_options = ChromeOptions()
+
+    driver = Chrome(service=service, options=chrome_options)
+
+    return driver
