000fb3085d8bd93278e6c76134f1e552a2e0ff54
Muhammad Naeem <inaeem@protonmail.com>
2023-06-25 19:55:18 +0500

    update driver configs constants

--- configs.py
+++ configs.py
@@ -8,18 +8,18 @@ import os
 load_dotenv()
 
 
-MAX_TABS = 3
+MAX_TABS = 2
 MAX_TRAVERSES = 2
 ADVERTISEMENT_PAUSE_TIME = 60
-
-HEADLESS = False
-
 
 DRIVER_EXECUTABLE_PATH = os.environ.get('DRIVER_EXECUTABLE_PATH', "./assets/chromedriver.exe")
 USER_AGENTS_FILE_PATH = os.environ.get("USER_AGENTS_FILE_PATH", "./assets/user_agents.json")
 PROXIES_FILE_PATH = os.environ.get("PROXIES_FILE_PATH", "./assets/proxies.json")
 PROXIES_USERNAME = os.environ.get("PROXIES_USERNAME", None)
 PROXIES_PASSWORD = os.environ.get("PROXIES_PASSWORD", None)
+LOADING_STRATEGY = "normal"  # eager | normal
+CHROME_HEADLESS = False  # True | False
+DONT_LOAD_IMAGES = False  # True | False
 
 
 ADVERTISEMENT_SELECTORS = [
